a7b9e098e6d05bae6f14adf10e35e4bd3aa034e7
riskinputra <riski.n.putra@gmail.com>
2020-08-10 21:18:45 +0700

    html template

---
+++ src/data/charities.js
@@ -0,0 +1,18 @@
+module.exports = {
+  charities: [
+    {
+      id: 1,
+      title: "First Charity Project",
+      category: "Money",
+      thumbnail:
+        "https://imgix.bustle.com/uploads/image/2019/5/2/ffa82ad4-937e-412c-9bfd-33cb9252e88e-instagram-donate.jpg?w=1020&h=576&fit=crop&crop=faces&auto=format%2Ccompress&cs=srgb&q=70",
+      pledged: 9900,
+      target: 10000,
+      date_end: +new Date("10 August 2020"),
+      profile_photo:
+        "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+      profile_name: "Viroide Bueno",
+      no_pledges: 0,
+    },
+  ],
+};
---
+++ src/pages/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { charities } from "../data/charities";
+  import Header from "../components/Header.svelte";
+  import Welcome from "../components/Welcome.svelte";
+  import CharityList from "../components/CharityList.svelte";
+  import Promo from "../components/Promo.svelte";
+  import Footer from "../components/Footer.svelte";
+  // your script goes here
+</script>
+
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
